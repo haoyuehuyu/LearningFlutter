764b11cf07b02ff97a16382c608d45a824def7bb
Reed <hjphfut@163.com>
2019-07-22 00:15:05 +0800

    添加三方库使用

---
+++ flutter_first_basic_1/lib/use_url_launcher.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+class UseURLLauncherPage extends StatefulWidget {
+  UseURLLauncherPage({Key key}) : super(key: key);
+
+  _UseURLLauncherPageState createState() => _UseURLLauncherPageState();
+}
+
+class _UseURLLauncherPageState extends State<UseURLLauncherPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Use url_launcher"),
+      ),
+      body: Center(
+        child: RaisedButton(
+          onPressed: () {
+            var baiduURL = "https://www.baidu.com";
+            launch(baiduURL);
+          },
+          child: Text("打开百度"),
+        ),
+      ),
+    );
+  }
+}
